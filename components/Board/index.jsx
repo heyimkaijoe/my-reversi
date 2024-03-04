5280fe1bd97c30ba760c57187b944dbdababe2c9
kchsu <kevin3117213@gmail.com>
2024-03-04 16:18:14 +0800

    Add: count dark/light disk num

--- components/Board/index.jsx
+++ components/Board/index.jsx
@@ -10,12 +10,15 @@ export default function Board({ currentDisks, darkIsNext, handlePlay }) {
     const boardRows = chunks.map((chunk, idx) => {
         return <BoardRow key={idx} row={idx} disks={chunk} handlePlay={handlePlay} />;
     });
-    let status = "Next player: " + (darkIsNext ? "Dark" : "Light");
+    const status = "Next player: " + (darkIsNext ? "Dark" : "Light");
+    const countDisksByType = (isDarkDisk) => currentDisks.filter((disk) => disk === isDarkDisk).length;
+    const scores = "Dark: " + countDisksByType(true) + ", Light: " + countDisksByType(false);
 
     return (
-        <>
+        <div className="flex flex-col">
             <div className="text-3xl mb-2">{status}</div>
+            <div className="text-2xl mb-2">{scores}</div>
             <div>{boardRows}</div>
-        </>
+        </div>
     );
 }
--- components/Game/index.jsx
+++ components/Game/index.jsx
@@ -7,7 +7,6 @@ export default function Game() {
     const [currentMove, setCurrentMove] = useState(0);
     const darkIsNext = currentMove % 2 === 0;
     const currentDisks = history[currentMove];
-    const nextDisks = currentDisks.slice();
     const handlePlay = (row, col) => {
         const diskIdx = calcDiskIdx(row, col);
         console.log(`${diskIdx} Clicked`);
@@ -27,8 +26,12 @@ export default function Game() {
 
     let flippable = [];
     let temp = [];
+    const findFlippable = (diskIdx) => {
+        if (currentDisks[diskIdx] === null || currentDisks[diskIdx] === undefined || (temp.filter((idx) => currentDisks[idx] === darkIsNext).length === 1)) return;
+        return temp.push(diskIdx);
+    };
     const collectFlippable = () => {
-        if (!(temp.every((idx) => nextDisks[idx] === !darkIsNext))) flippable = flippable.concat(temp.slice(0, temp.length - 1));
+        if (!(temp.every((idx) => currentDisks[idx] === !darkIsNext))) flippable = flippable.concat(temp.slice(0, temp.length - 1));
         temp = [];
     };
     const flipDisks = (row, col) => {
@@ -38,16 +41,14 @@ export default function Game() {
         // horizontal +
         for (let i = col + 1; i <= maxDiskIdx; i++) {
             let diskIdx = calcDiskIdx(row, i);
-            if (nextDisks[diskIdx] === null || nextDisks[diskIdx] === undefined || (temp.filter((idx) => nextDisks[idx] === darkIsNext).length === 1)) break;
-            temp.push(diskIdx);
+            if (findFlippable(diskIdx) === undefined) break;
         };
         console.log(`hor +: ${temp}`);
         collectFlippable();
         // horizontal -
         for (let i = col - 1; i >= minDiskIdx; i--) {
             let diskIdx = calcDiskIdx(row, i);
-            if (nextDisks[diskIdx] === null || nextDisks[diskIdx] === undefined || (temp.filter((idx) => nextDisks[idx] === darkIsNext).length === 1)) break;
-            temp.push(diskIdx);
+            if (findFlippable(diskIdx) === undefined) break;
         };
         console.log(`hor -: ${temp}`);
         collectFlippable();
@@ -55,17 +56,14 @@ export default function Game() {
         // vertical +
         for (let i = row + 1; i <= maxDiskIdx; i++) {
             let diskIdx = calcDiskIdx(i, col);
-            if (nextDisks[diskIdx] === null || nextDisks[diskIdx] === undefined || (temp.filter((idx) => nextDisks[idx] === darkIsNext).length === 1)) break;
-            temp.push(diskIdx);
+            if (findFlippable(diskIdx) === undefined) break;
         }
         console.log(`ver +: ${temp}`);
-        if (!(temp.every((idx) => nextDisks[idx] === !darkIsNext))) flippable = flippable.concat(temp.slice(0, temp.length - 1));
-        temp = [];
+        collectFlippable();
         // vertical -
-        for (let i = row - 1; row >= minDiskIdx; i--) {
+        for (let i = row - 1; i >= minDiskIdx; i--) {
             let diskIdx = calcDiskIdx(i, col);
-            if (nextDisks[diskIdx] === null || nextDisks[diskIdx] === undefined || (temp.filter((idx) => nextDisks[idx] === darkIsNext).length === 1)) break;
-            temp.push(diskIdx);
+            if (findFlippable(diskIdx) === undefined) break;
         };
         console.log(`ver -: ${temp}`);
         collectFlippable();
@@ -76,20 +74,17 @@ export default function Game() {
             let nextCol = col - i;
             let diskIdx = calcDiskIdx(nextRow, nextCol);
 
-            if (nextDisks[diskIdx] === null || nextDisks[diskIdx] === undefined || (temp.filter((idx) => nextDisks[idx] === darkIsNext).length === 1)) break;
-                temp.push(diskIdx);
+            if (findFlippable(diskIdx) === undefined) break;
         }
         console.log(`pos slope +: ${temp}`);
-        if (!(temp.every((idx) => nextDisks[idx] === !darkIsNext))) flippable = flippable.concat(temp.slice(0, temp.length - 1));
-        temp = [];
+        collectFlippable();
         // positive slope -
         for (let i = 1; i <= 7; i++) {
             let nextRow = row - i;
             let nextCol = col + i;
             let diskIdx = calcDiskIdx(nextRow, nextCol);
 
-            if (nextDisks[diskIdx] === null || nextDisks[diskIdx] === undefined || (temp.filter((idx) => nextDisks[idx] === darkIsNext).length === 1)) break;
-                temp.push(diskIdx);
+            if (findFlippable(diskIdx) === undefined) break;
         }
         console.log(`pos slope -: ${temp}`);
         collectFlippable();
@@ -100,8 +95,7 @@ export default function Game() {
             let nextCol = col + i;
             let diskIdx = calcDiskIdx(nextRow, nextCol);
 
-            if (nextDisks[diskIdx] === null || nextDisks[diskIdx] === undefined || (temp.filter((idx) => nextDisks[idx] === darkIsNext).length === 1)) break;
-                temp.push(diskIdx);
+            if (findFlippable(diskIdx) === undefined) break;
         }
         console.log(`neg slope +: ${temp}`);
         collectFlippable();
@@ -111,15 +105,15 @@ export default function Game() {
             let nextCol = col - i;
             let diskIdx = calcDiskIdx(nextRow, nextCol);
 
-            if (nextDisks[diskIdx] === null || nextDisks[diskIdx] === undefined || (temp.filter((idx) => nextDisks[idx] === darkIsNext).length === 1)) break;
-                temp.push(diskIdx);
+            if (findFlippable(diskIdx) === undefined) break;
         }
         console.log(`neg slope -: ${temp}`);
         collectFlippable();
-        // TODO: make duplicates into functions
+
         // TODO: make back(last move) button
         console.log(`flippable: ${flippable}`);
         if (flippable.length !== 0) {
+            const nextDisks = currentDisks.slice();
             flippable.forEach((num) => {
                 nextDisks[num] = darkIsNext;
             });
@@ -129,12 +123,21 @@ export default function Game() {
             return;
         };
     };
+    const jumpToLastMove = () => {
+        setHistory(history.slice(0, -1));
+        setCurrentMove(currentMove - 1);
+    };
 
     return (
-        <Board currentDisks={currentDisks} darkIsNext={darkIsNext} handlePlay={handlePlay}/>
+        <div className="flex justify-center">
+            <Board currentDisks={currentDisks} darkIsNext={darkIsNext} handlePlay={handlePlay}/>
+            <button onClick={jumpToLastMove} className="ml-4 border rounded p-2 cursor-pointer">Last Move</button>
+        </div>
     );
 }
 
 function calcDiskIdx(row, col) {
-    return row * 8 + col;
+    if ((0 <= row && row <= 7) && (0 <= col && col <= 7)) {
+        return row * 8 + col;
+    };
 }
