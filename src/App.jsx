7254958372714eb17b46942dcca43a5970cffccc
kchsu <kevin3117213@gmail.com>
2024-03-05 23:18:35 +0800

    Modify: html head title, spaces in classNames & TODOs

--- src/App.jsx
+++ src/App.jsx
@@ -11,3 +11,8 @@ export default function App() {
     //   Square: 64(8x8)
     // 
     // States needed: history, currentMove
+// TODO: remove component App -> use Game component as top-level component is fine
+// TODO: check if there're any places that can pass JSX as children
+// ref: https://react.dev/learn/passing-props-to-a-component#passing-jsx-as-children
+// hover effect -> onPointerEnter/onPointerLeave
+// return <>{someCondition && <Component />}</>
